900a1745d1bc9aa0cc12384a5eec8b6939355a55
RodrigoJose-Silva <digo.1903@hotmail.com>
2021-06-14 06:57:10 -0300

    Criado função if e else; criado função case; Criado função for; Utilizado a classe Scanner para ler dados inseridos pelo teclado

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -3,19 +3,84 @@ package intro;
 
 // 2 - Referências as bibliotecas
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 // 3 - Classe
 public class Medidas {
     // 3.1 - Atributos - Carcterísticas
 
     // 3.2 - Metodos e Funções
     public static void main(String[] args){
-        System.out.println("Calculo de Areas");
-        calcularAreaModoCompacto();
-        calcularAreaModoExtenso();
 
+        // Condicional = verificar uma condição - fazer uma pergunta para uma pessoa, um hardware ou software
+
+        // Utilizar a classe SCANNER do Java para ler a escollha do usuário no console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("M E N U    D E    O P Ç Õ E S");
+        System.out.println("C - Calcular Area Compacta");
+        System.out.println("D - Contar Até Dez Crescente");
+        System.out.println("E - Calcular Area Extensa");
+        System.out.println("F - Contar Até Dez Decrescente");
+        System.out.println("I - If Simples");
+        System.out.println("Digite a opção desejada: ");
+        String opcao = scanner.next();
+
+        //switch - selecionar o comportamento do programa coforme a escolha da pessoa ou software
+
+        //String opcao = "extenso";
+
+        switch (opcao){
+            case "c":
+            case "C":
+                System.out.println("Você escolheu executar o método calcularAreaModoCompacto");
+                calcularAreaModoCompacto();
+                break;
+            case "d":
+            case "D":
+                System.out.println("Você escolheu executar o método  crescente contarAteDez");
+                contarAteDez();
+                break;
+            case "f":
+            case "F":
+                System.out.println("Você escolheu executar o método decrescente contarAteDez");
+                contarAteDezRegressiva();
+                break;
+            case "e":
+            case "E":
+                System.out.println("Você escolheu executar o método calcularAreaModoExtenso");
+                calcularAreaModoExtenso();
+                break;
+            case "i":
+            case "I":
+                System.out.println("Você escolheu executar o método ifSimples");
+                ifSimples();
+                break;
+            default:
+                System.out.println("Você escolheu executar um método inexistente!");
+                break;
+        }
+}
+
+    public static void ifSimples(){
+
+        // Condicional = verificar uma condição - fazer uma pergunta para uma pessoa, um hardware ou software
+
+        // if = se
+        //else = senão
+
+        String modo = "compacto";
+        if (modo == "compacto"){
+            calcularAreaModoCompacto();
+        }
+        else{
+            calcularAreaModoExtenso();
+        }
     }
+
     public static void calcularAreaModoExtenso(){
 
+        System.out.println("Calculo de Areas");
         System.out.println("Calculo de Areas Extenso");
 
         //Calculo de área - Ex.: tamanho do tapete ou piso
@@ -24,7 +89,7 @@ public class Medidas {
         int resultado;
 
         largura = 5; // largura recebe valor 4
-        comprimento = 3; // comprimento receber valor 3
+        comprimento = 5; // comprimento receber valor 3
 
         resultado = largura * comprimento;
         System.out.println("Para a largura de "+largura+ "m e o comprimento de "
@@ -34,6 +99,7 @@ public class Medidas {
 
     public static void calcularAreaModoCompacto(){
 
+        System.out.println("Calculo de Areas");
         System.out.println("Calculo de Areas Compacto");
 
         //Calculo de área reduzido
@@ -43,5 +109,23 @@ public class Medidas {
         System.out.println("Para a largura de "+largura+ "m e o comprimento de "
                 +comprimento+"m a área é de " + largura * comprimento + "m².");
     }
+    public static void contarAteDez(){
 
+        // Loops ou Repetições
+        // for = repetição incondicional
+
+        System.out.println("Contagem crescrente");
+
+        for (int numero = 1; numero <= 10; numero++){
+            System.out.println("Numero: " + numero);
+        }
+    }
+
+    public static void contarAteDezRegressiva(){
+
+        System.out.println("Contegem Regressiva");
+
+        for (int numero = 10; numero > -1; numero--)
+            System.out.println("Numero: " + numero);
+    }
 }
